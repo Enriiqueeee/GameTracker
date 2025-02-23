306057fa2ee3a3d5f51781d2e5af7cb1c6872af0
Enrique Arenas <94898081+Enriiqueee@users.noreply.github.com>
2025-02-23 23:57:17 +0100

    Implementado el bottomNavigation y los fragmentos

---
+++ app/src/main/java/edu/iesam/gametracker/features/played/presentation/PlayedFragment.kt
@@ -0,0 +1,7 @@
+package edu.iesam.gametracker.features.played.presentation
+
+import androidx.fragment.app.Fragment
+import edu.iesam.gametracker.R
+
+class PlayedPresentation(): Fragment(R.layout.played_fragment) {
+}
