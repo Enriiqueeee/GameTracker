244767e00b84f17332b10348e84aeaf4d65b7d6b
Enrique Arenas <94898081+Enriiqueee@users.noreply.github.com>
2025-02-26 00:05:55 +0100

    Agregado del bottomNavigation + fragmentos para cada uno

---
+++ app/src/main/java/edu/iesam/gametracker/features/setting/presentation/SettingFragment.kt
@@ -0,0 +1,7 @@
+package edu.iesam.gametracker.features.setting.presentation
+
+import androidx.fragment.app.Fragment
+import edu.iesam.gametracker.R
+
+class SettingFragment() : Fragment(R.layout.fragment_setting) {
+}
